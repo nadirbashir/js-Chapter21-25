11766cb66778c37c0e42765b3233e45b2fb33719
nadirbashir <1.nadirbashir@gmail.com>
2021-05-19 01:03:11 +0500

    chapter 21-25

---
+++ app.js
@@ -0,0 +1,51 @@
+  /************Quest 1. **********/
+  
+  // var firstName = prompt("Enter Your First Name: ");
+  // var lastName = prompt("Enter your Last Name: ");
+  
+  // alert("Welcome! "+firstName.concat(" "+lastName))
+ 
+  /************Quest. 2 **********/
+ 
+//  var inp = prompt("Enter your favourite Mobile Phone Model") 
+//  document.write("My favourite phone is " + inp + "<br>" + "Length sof string is: " + inp.length)    
+
+  /************Quest. 3 **********/
+  
+//   var word = "Pakistani";
+//   document.write("String: "+word); 
+//   document.write("<br>Index of 'n': "+word.indexOf("n")); 
+
+/************Quest. 4 **********/
+
+//   var word = "Hello World!" ;
+//   document.write("String: "+word);
+//   document.write("<br>Last Index of 'l': "+word.lastIndexOf("l")); 
+
+
+  /************Quest. 5 **********/
+  
+//   var word = "Pakistani";
+//   document.write("String: "+word); 
+//   document.write("<br>Chrater at index '3': "+word.charAt(3)); 
+  
+  
+  /************Quest. 7 **********/
+  
+//   var word = "Hyderabad";
+//   document.write("String: "+word); 
+//   document.write("<br>replaced Word: "+word.replace("Hyder","Islam")); 
+
+//**************Quest. 8 *********/
+
+// var sentence = "Ibrahim and Asad are best friends, They play cricket and Football";
+// document.write("Sentence: " + sentence.replace(/and/g,"&"));
+
+/**********Quest. 9 ********/
+
+var num1 = "475"
+var num2 = 475
+document.write("value 1: "+ num1 + "<br>")
+document.write("Type: "+ typeof(num1) + "<br>")
+document.write("value 2: "+ num2 + "<br>")
+document.write("Type: "+ typeof(num2))
